3dec2f82fedbe5883c7e892dd89f369f36a0c699
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2013-07-02 15:35:56 +0530

    fixed errors:  multiple chnage nickname prompt boxes after multiple create new #3  When new pad created, all the writes will be send back to the other sessions #1

--- client/views/pad.js
+++ client/views/pad.js
@@ -1,6 +1,7 @@
+var pad;
+var remoteUser;
+
 Meteor.startup(function() {
-  var pad;
-  var remoteUser;
   Deps.autorun(function() {
     if(pad) {
       pad.close();
@@ -10,5 +11,23 @@ Meteor.startup(function() {
     var padId = Session.get('padId');
     pad = new Pad(padId);
     remoteUser = new RemoteUser(padId, pad);
+  });
+});
+
+$(function() {
+  $('body').on('click', '#wipe', function() {
+    pad.wipe(true);
+  });
+
+  $('body').on('click', '#set-nickname', function() {
+    var name = prompt('Enter your nickname');
+    if(name && name.trim() != '') {
+      pad.setNickname(name);
+    }
+  });
+
+  $('body').on('click', '#create-new', function() {
+    var newPadId = Random.id();
+    Meteor.Router.to('pad', newPadId);
   });
 });
