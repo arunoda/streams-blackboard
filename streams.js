ca6130ce4a3d1d1c65fa4e17ed583605faddf000
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2013-07-05 03:02:58 +0530

    updated permissions

--- streams.js
+++ streams.js
@@ -1,7 +1,6 @@
 LineStream = new Meteor.Stream('lines');
 
 if(Meteor.isServer) {
-
   var subscriptionPads = {};
   LineStream.on('pad', function(padId) {
     var subscriptionId = this.subscriptionId;
@@ -21,7 +20,7 @@ if(Meteor.isServer) {
       //only allows events with padId to read from the stream
       return false;
     }
-  });
+  }, false);
 
   LineStream.permissions.write(function(event) {
     return true;
